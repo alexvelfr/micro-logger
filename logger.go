2e5bbbe64e89d24990144e943fd1217cc3c2da1f
Alex <kudrushow@gmail.com>
2021-12-28 13:49:06 +0200

    feat: console mode

--- logger.go
+++ logger.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"errors"
 	"io"
+	"log"
 
 	logstashclientmicro "github.com/alexvelfr/logstash-client-micro"
 )
 
 var ErrLogNotInit = errors.New("logger not init, please run InitLogger first")
 var logClient logstashclientmicro.Client
+var consoleMode = false
 
 // InitLogger init logstash logger client
 func InitLogger(servceName, uri string, useInsecureSSL bool) {
@@ -22,6 +24,14 @@ func LogError(reqID, action, file, data string, err error) error {
 
 func LogInfo(reqID, action, file, data string) error {
 	return logCommon(reqID, action, file, data, logstashclientmicro.Info, nil)
+}
+
+func EnabldeConsoleMode() {
+	consoleMode = true
+}
+
+func DisableConsoleMode() {
+	consoleMode = false
 }
 
 func LogDebug(reqID, action, file, data string) error {
@@ -39,6 +49,17 @@ func LogErrorStrict(err error) {
 func logCommon(reqID, action, file, data string, t logstashclientmicro.LogType, err error) error {
 	if logClient == nil {
 		return ErrLogNotInit
+	}
+	if consoleMode {
+		log.Printf("XReqID=%s;\nData=%s;\nFile=%s;\nAction=%s;\nErr=%s;\nType=%s\n",
+			reqID,
+			data,
+			file,
+			action,
+			err,
+			t,
+		)
+		return nil
 	}
 	return logClient.LogError(context.Background(), logstashclientmicro.Message{
 		XReqID: reqID,
